f2a8daf41ebe1a1a4c53a9d5a710347bf3a5a9ad
Joe Essey <joeessey@joes-air.attlocal.net>
2017-12-09 15:32:14 -0500

    add some nested partialling, not perfect

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,6 +1,5 @@
 // frontend/packs/application.js
-// import "./application.css";
 import "init";
 import "components/page/page";
+import "components/songs/songs";
 import "components/song/song";
-// document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
